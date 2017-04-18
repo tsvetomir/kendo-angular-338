951c2da6936c1b8181bc1345fd2fca5a94223a65
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2017-04-18 18:09:03 +0300

    feat: add charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  chartSeries = [{
+    dataPoints: [{
+      amount: 10,
+      period: new Date('2016/01/01')
+    }, {
+      amount: 15,
+      period: new Date('2016/01/02')
+    }]
+  }, {
+    dataPoints: [{
+      amount: 20,
+      period: new Date('2016/01/01')
+    }, {
+      amount: 25,
+      period: new Date('2016/01/02')
+    }]
+  }];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
+import { ChartsModule } from '@progress/kendo-angular-charts';
 
 import { AppComponent } from './app.component';
 
@@ -11,6 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ChartsModule,
     FormsModule,
     HttpModule
   ],
